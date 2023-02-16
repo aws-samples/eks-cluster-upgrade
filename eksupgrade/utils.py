61024f56f33e24309b9b7a593a4f8c83c89f627d
Mark Beacom <7315957+mbeacom@users.noreply.github.com>
2023-02-16 13:59:16 -0500

    Adjust pathing since module move

--- eksupgrade/utils.py
+++ eksupgrade/utils.py
@@ -6,18 +6,18 @@ import pkgutil
 import yaml
 
 
-def get_package_asset(filename: str, base_path: str = "S3Files/") -> str:
+def get_package_asset(filename: str, base_path: str = "src/S3Files/") -> str:
     """Get the specified package asset data."""
     return pkgutil.get_data(__package__, f"{base_path}/{filename}").decode("utf-8")
 
 
-def get_package_dict(filename: str, base_path: str = "S3Files/"):
+def get_package_dict(filename: str, base_path: str = "src/S3Files/"):
     """Get the specified package asset data dictionary."""
     _data = get_package_asset(filename, base_path)
     return json.loads(_data)
 
 
-def get_package_yaml(filename: str, base_path: str = "S3Files/"):
+def get_package_yaml(filename: str, base_path: str = "src/S3Files/"):
     """Get the specified package asset data YAML."""
     _data = get_package_asset(filename, base_path)
     return yaml.safe_load(_data)
