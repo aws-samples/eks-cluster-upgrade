8478331574113fb89c8db7f61b6684c1ccd818e4
Manu Chandrasekhar <manuchn@amazon.com>
2023-02-16 13:46:16 -0500

    fix: removed format specifiers

--- eksupgrade/src/latest_ami.py
+++ eksupgrade/src/latest_ami.py
@@ -14,15 +14,15 @@ def get_latest_ami(cluster_version: str, instance_type: str, image_to_search: st
     client = boto3.client("ec2", region_name=region)
 
     if "Amazon Linux 2" in instance_type:
-        names = [f"/aws/service/eks/optimized-ami/{cluster_version: s}/amazon-linux-2/recommended/image_id"]
+        names = [f"/aws/service/eks/optimized-ami/{cluster_version}/amazon-linux-2/recommended/image_id"]
     elif "Windows" in instance_type:
-        names = [f"/aws/service/ami-windows-latest/{image_to_search}-{cluster_version: s}/image_id"]
+        names = [f"/aws/service/ami-windows-latest/{image_to_search}-{cluster_version}/image_id"]
     elif "bottlerocket" in instance_type.lower():
-        names = [f"/aws/service/bottlerocket/aws-k8s-{cluster_version: s}/x86_64/latest/image_id"]
+        names = [f"/aws/service/bottlerocket/aws-k8s-{cluster_version}/x86_64/latest/image_id"]
     elif "Ubuntu" in instance_type:
         filters = [
             {"Name": "owner-id", "Values": ["099720109477"]},
-            {"Name": "name", "Values": [f"ubuntu-eks/k8s_{cluster_version: s}*"]},
+            {"Name": "name", "Values": [f"ubuntu-eks/k8s_{cluster_version}*"]},
             {"Name": "is-public", "Values": ["true"]},
         ]
         response = client.describe_images(Filters=filters)
