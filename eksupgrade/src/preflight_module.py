e4f5ba57671862ff07fbde9fcc478714a695a1aa
Mark Beacom <7315957+mbeacom@users.noreply.github.com>
2023-02-16 13:54:00 -0500

    fix: Package pathing issues for static files (#53)

--- eksupgrade/src/preflight_module.py
+++ eksupgrade/src/preflight_module.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import json
 import logging
+import pkgutil
 from typing import Any, Dict, List
 
 import boto3
@@ -10,6 +11,8 @@ import urllib3
 import yaml
 from kubernetes import client
 from kubernetes.client import *
+
+from eksupgrade.utils import get_package_dict
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from .k8s_client import get_default_version, loading_config
@@ -207,8 +210,7 @@ def cluster_roles(
 ) -> None:
     """Get cluster roles."""
     loading_config(cluster_name, region)
-    with open("eksupgrade/src/S3Files/cluster_roles.json", "r", encoding="utf-8") as f:
-        cluster_roles_list = json.load(f)
+    cluster_roles_list = get_package_dict("cluster_roles.json")
 
     if preflight:
         cluster_roles_list = cluster_roles_list["preflight"]
@@ -327,9 +329,7 @@ def addon_version(
     cni_target_version: str = get_default_version("vpc-cni", **default_version_kwargs).split("-")[0].lstrip("v")
 
     # Kube Proxy config
-    with open("eksupgrade/src/S3Files/kube-proxy.json", "r", encoding="utf-8") as f:
-        kube_proxy_config = json.load(f)
-
+    kube_proxy_config = get_package_dict("kube-proxy.json")
     kube_proxy_container = kube_proxy_config["spec"]["template"]["spec"]["containers"][0]
     yaml_data["kube-proxy"] = {
         "image": kube_proxy_container["image"],
@@ -338,9 +338,7 @@ def addon_version(
     }
 
     # Core DNS config
-    with open("eksupgrade/src/S3Files/coredns.json", "r", encoding="utf-8") as f:
-        core_dns_config = json.load(f)
-
+    core_dns_config = get_package_dict("coredns.json")
     coredns_container = core_dns_config["spec"]["template"]["spec"]["containers"][0]
     yaml_data["coredns"] = {
         "image": coredns_container["image"],
@@ -349,9 +347,7 @@ def addon_version(
     }
 
     # VPC CNI config
-    with open("eksupgrade/src/S3Files/vpc-cni.json", "r", encoding="utf-8") as f:
-        vpc_cni_config = json.load(f)
-
+    vpc_cni_config = get_package_dict("vpc-cni.json")
     vpc_cni_container = vpc_cni_config["spec"]["template"]["spec"]["containers"][0]
     yaml_data["vpc-cni"] = {
         "image": vpc_cni_container["image"],
@@ -360,9 +356,7 @@ def addon_version(
     }
 
     # Kube Proxy config map
-    with open("eksupgrade/src/S3Files/kube-proxy-configmap.json", "r", encoding="utf-8") as f:
-        kube_proxy_config_map = json.load(f)
-
+    kube_proxy_config_map = get_package_dict("kube-proxy-configmap.json")
     config_map["certificate-authority"] = yaml.safe_load(kube_proxy_config_map["data"]["kubeconfig"])["clusters"][0][
         "cluster"
     ]["certificate-authority"]
---
+++ eksupgrade/utils.py
@@ -0,0 +1,23 @@
+"""Define module level utilities to be used across the EKS Upgrade package."""
+
+import json
+import pkgutil
+
+import yaml
+
+
+def get_package_asset(filename: str, base_path: str = "S3Files/") -> str:
+    """Get the specified package asset data."""
+    return pkgutil.get_data(__package__, f"{base_path}/{filename}").decode("utf-8")
+
+
+def get_package_dict(filename: str, base_path: str = "S3Files/"):
+    """Get the specified package asset data dictionary."""
+    _data = get_package_asset(filename, base_path)
+    return json.loads(_data)
+
+
+def get_package_yaml(filename: str, base_path: str = "S3Files/"):
+    """Get the specified package asset data YAML."""
+    _data = get_package_asset(filename, base_path)
+    return yaml.safe_load(_data)
